936ab6fa0030fcdb01c06c1e90cc82f0195d3f3b
riawiththesam <riawiththesam@gmail.com>
2023-08-04 02:04:35 +0900

    feat: 壁設定時に両側に設定する

--- src/use-case/dungeon-map-use-case/dungeon-map-use-case.ts
+++ src/use-case/dungeon-map-use-case/dungeon-map-use-case.ts
@@ -20,12 +20,30 @@ currentMapState.next({
 });
 
 export function useDungeonMapUseCase() {
+  /**
+   * マップの対応する座標に全壁(どちらから見ても壁となる壁)
+   *
+   * @param xIndex
+   * @param yIndex
+   * @param direction
+   */
   function setWall(xIndex: number, yIndex: number, direction: DungeonWallDirection) {
+    const backSide = getBackSideWall(xIndex, yIndex, direction);
+
     const nextList = currentMapState.value.mapChipList.map((row, rowIndex) => {
       return row.map((col, colIndex) => {
-        const nextWalls = { ...col.walls };
-        nextWalls[direction] = "wall";
-        if (colIndex == xIndex && rowIndex == yIndex) return { ...col, walls: nextWalls };
+        // 指定した座標の壁
+        if (colIndex == xIndex && rowIndex == yIndex) {
+          const nextWalls = { ...col.walls };
+          nextWalls[direction] = "wall";
+          return { ...col, walls: nextWalls };
+        }
+        // 指定した座標の裏側の壁
+        if (colIndex == backSide.xIndex && rowIndex == backSide.yIndex) {
+          const nextWalls = { ...col.walls };
+          nextWalls[backSide.direction] = "wall";
+          return { ...col, walls: nextWalls };
+        }
         return col;
       });
     });
@@ -37,4 +55,47 @@ export function useDungeonMapUseCase() {
     currentMapObservable,
     setWall,
   };
+}
+
+type GetBackSideWallResult = {
+  xIndex: number;
+  yIndex: number;
+  direction: DungeonWallDirection;
+};
+
+/**
+ * 対象の座標に存在する壁の裏側の壁の座標を取得する
+ *
+ * @param xIndex
+ * @param yIndex
+ * @param direction
+ * @returns
+ */
+function getBackSideWall(xIndex: number, yIndex: number, direction: DungeonWallDirection): GetBackSideWallResult {
+  switch (direction) {
+    case "west":
+      return {
+        xIndex: xIndex - 1,
+        yIndex: yIndex,
+        direction: "east",
+      };
+    case "east":
+      return {
+        xIndex: xIndex + 1,
+        yIndex: yIndex,
+        direction: "west",
+      };
+    case "north":
+      return {
+        xIndex: xIndex,
+        yIndex: yIndex - 1,
+        direction: "south",
+      };
+    case "south":
+      return {
+        xIndex: xIndex,
+        yIndex: yIndex + 1,
+        direction: "north",
+      };
+  }
 }
