40522c92a4ed60d7b76898e3d8b22b2e588bbd64
riawiththesam <riawiththesam@gmail.com>
2023-08-04 03:29:25 +0900

    chore: analyze command

--- vite.config.js
+++ vite.config.js
@@ -1,18 +1,30 @@
 import { resolve } from "path";
 import { defineConfig } from "vite";
+import { visualizer } from "rollup-plugin-visualizer";
 
 const root = resolve(__dirname, "src");
 const outDir = resolve(__dirname, "dist");
 
-export default defineConfig({
-  root,
-  build: {
-    outDir,
-    rollupOptions: {
-      input: {
-        app: resolve(root, "app", "index.html"),
-        tool: resolve(root, "tool", "index.html"),
+export default defineConfig(({ mode }) => {
+  return {
+    root,
+    build: {
+      outDir,
+      rollupOptions: {
+        input: {
+          app: resolve(root, "app", "index.html"),
+          tool: resolve(root, "tool", "index.html"),
+        },
+        plugins: [
+          mode === "analyze" &&
+            visualizer({
+              open: true,
+              filename: "dist/stats.html",
+              gzipSize: true,
+              brotliSize: true,
+            }),
+        ],
       },
     },
-  },
+  };
 });
