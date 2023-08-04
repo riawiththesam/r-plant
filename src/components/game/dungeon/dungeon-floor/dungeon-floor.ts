4af3d1362438a6fd289027acd822195521b56736
riawiththesam <riawiththesam@gmail.com>
2023-08-05 05:37:07 +0900

    feat: floor color

--- src/components/game/dungeon/dungeon-floor/dungeon-floor.ts
+++ src/components/game/dungeon/dungeon-floor/dungeon-floor.ts
@@ -1,18 +1,19 @@
-import * as PIXI from "pixi.js";
+import { Container, Color, Graphics } from "pixi.js";
 
 export type DungeonFloorProps = {
   chipSize: number;
   visible: boolean;
 };
 
-export class DungeonFloor extends PIXI.Container {
+export class DungeonFloor extends Container {
   constructor(props: DungeonFloorProps) {
     super();
     const { chipSize, visible } = props;
     this.visible = visible;
 
-    const rect = new PIXI.Graphics();
-    rect.beginFill(0x0000ff);
+    const color = new Color({ r: 90, g: 100, b: 60 });
+    const rect = new Graphics();
+    rect.beginFill(color);
     rect.drawRect(-chipSize / 2, -chipSize / 2, chipSize, chipSize);
     this.addChild(rect);
   }
