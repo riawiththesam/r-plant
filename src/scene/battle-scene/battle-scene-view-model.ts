c1b0cf3bf07e5c8cb08932296910b08f8266985a
riawiththesam <riawiththesam@gmail.com>
2023-08-09 07:59:48 +0900

    feat: スライム表示

---
+++ src/scene/battle-scene/battle-scene-view-model.ts
@@ -0,0 +1,33 @@
+import { BehaviorSubject } from "rxjs";
+import slime from "../../game-assets/character/slime/slime.png";
+
+export type BattleCharacterState = {
+  image: string;
+  x: number;
+  y: number;
+  width: number;
+  height: number;
+};
+
+export type BattleCharacterListState = {
+  list: ReadonlyArray<BattleCharacterState>;
+};
+
+export class BattleSceneViewModel {
+  private enemyListSubject = new BehaviorSubject<BattleCharacterListState>({ list: [] });
+  enemyListObservable = this.enemyListSubject.asObservable();
+
+  load() {
+    this.enemyListSubject.next({
+      list: [
+        {
+          image: slime,
+          x: 100,
+          y: 100,
+          width: 100,
+          height: 100,
+        },
+      ],
+    });
+  }
+}
--- src/scene/battle-scene/battle-scene.ts
+++ src/scene/battle-scene/battle-scene.ts
@@ -1,16 +1,33 @@
-import { Sprite } from "pixi.js";
+import { Container, Sprite } from "pixi.js";
 import { Scene } from "../../util/pixi/scene/scene";
-import bgGrass from "../../game-assets/background/bg-grass.png";
 import { gameConfig } from "../../common/game-config";
+import bgGrass from "../../game-assets/background/bg-grass.png";
+import { BattleSceneViewModel } from "./battle-scene-view-model";
 
 export class BattleScene extends Scene {
-  constructor() {
-    super();
+  override onCreate() {
+    const viewModel = new BattleSceneViewModel();
 
     const background = Sprite.from(bgGrass, {
       width: gameConfig.width,
       height: gameConfig.height,
     });
     this.addChild(background);
+
+    const enemyContainer = new Container();
+    this.addChild(enemyContainer);
+    viewModel.enemyListObservable.subscribe((state) => {
+      enemyContainer.removeChildren();
+      state.list.forEach((item) => {
+        const enemySprite = Sprite.from(item.image);
+        enemySprite.x = item.x;
+        enemySprite.y = item.y;
+        enemySprite.width = item.width;
+        enemySprite.height = item.height;
+        enemyContainer.addChild(enemySprite);
+      });
+    });
+
+    viewModel.load();
   }
 }
