b3d91f41d664ccdeca9411064ff98077275dd7d8
riawiththesam <riawiththesam@gmail.com>
2023-08-15 07:40:55 +0900

    feat: 敵の攻撃のエフェクト設定

--- src/components/game/battle/battle-effect/battle-effect-layer.ts
+++ src/components/game/battle/battle-effect/battle-effect-layer.ts
@@ -3,6 +3,7 @@ import { type Observable, type Subscription } from "rxjs";
 import { type BattleSceneState } from "../../../../scene/battle-scene/battle-scene-subject";
 import { letSubscription } from "../../../../util/rxjs/subscription/subscriptions";
 import { BattleEffect } from "./battle-effect";
+import { getFriendPositionX, getFriendPositionY } from "../battle-friend/battle-friend-layer";
 
 export class BattleEffectLayer extends Container {
   private readonly battleEffect: BattleEffect;
@@ -24,13 +25,18 @@ export class BattleEffectLayer extends Container {
           const command = phase.allCharacterCommandList[phase.executingIndex];
           if (phase.commandEffectCurrentFrame !== 0 || command == null) return;
 
+          const targetIndex = command.targetList[0];
+          if (targetIndex == null) return;
           // TODO コマンド実行者actorTypeではなくコマンドの対象等で分岐する
           if (command.actorType === "friend") {
             // TODO 対象の数だけ表示などする
-            const targetIndex = command.targetList[0] ?? -1;
             const target = state.enemyListState.list[targetIndex];
             if (target == null) return;
             this.battleEffect.startAnimation(target.graphics.x, target.graphics.y);
+          } else {
+            const target = state.friendListState.list[targetIndex];
+            if (target == null) return;
+            this.battleEffect.startAnimation(getFriendPositionX(target), getFriendPositionY());
           }
         })
         .addTo(it);
